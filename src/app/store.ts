e21132fbafe66aef490b5bb31c1d2476e7890cb0
Drew Miley <drew.miley@newcastle.ac.uk>
2020-11-05 16:58:49 +0000

    WIP on patientsSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import patientsReducer from '../features/patients/patientsSlice';
 import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    patients: patientsReducer,
   },
 });
 
--- src/features/patients/patientsSlice.tsx
+++ src/features/patients/patientsSlice.tsx
@@ -1,59 +1,33 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { AppThunk, RootState } from '../../app/store';
 
-interface CounterState {
-  value: number;
+interface PatientsState {
+  value: Array<string>;
 }
 
-const initialState: CounterState = {
-  value: 0,
-};
+const initialState: PatientsState = {
+  value: []
+}
 
-export const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
-  reducers: {
-    increment: state => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
+export const patientsSlice = createSlice({
+    name: 'patients',
+    initialState,
+    reducers: {
+      setPatients: (state, action: PayloadAction<Array<string>>) => {
+        state.value = action.payload;
+      },
     },
-    decrement: state => {
-      state.value -= 1;
-    },
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload;
-    },
-  },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { setPatients } = patientsSlice.actions;
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched
-export const incrementAsync = (amount: number): AppThunk => async dispatch => {
-  const response: Response = await fetch('https://randomuser.me/api/');
-  const randomUser = await response.json();
-  const randomAge: number = randomUser.results[0].dob.age;
-  setTimeout(() => {
-    dispatch(incrementByAmount(randomAge));
-  }, 1000);
-};
+export const fetchUserIDs = (): AppThunk => async dispatch => {
+  const response: Response = await fetch('https://localhost:8000/inventory/users');
+  const results = await response.json();
+  console.log(results);
+  dispatch(setPatients(['USER1', 'USER2']));
+}
 
-// export const fetchUserIDs = (userIDs): AppThunk => async dispatch => {
-//   const response: Response = await fetch('https://localhost:8000/inventory/users');
-//   const results = await response.json();
-//   console.log(results)
-// }
+export const selectPatients = (state: RootState) => state.patients.value;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state: RootState) => state.counter.value;
-
-export default counterSlice.reducer;
+export default patientsSlice.reducer;
