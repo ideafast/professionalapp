7f3e7f94a3b63140dcf2940673bdefa11b269ddb
Drew Miley <drew.miley@newcastle.ac.uk>
2020-11-05 16:40:46 +0000

    Removes commented out code from counterSlice

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -45,12 +45,6 @@ export const incrementAsync = (amount: number): AppThunk => async dispatch => {
   }, 1000);
 };
 
-// export const fetchUserIDs = (userIDs): AppThunk => async dispatch => {
-//   const response: Response = await fetch('https://localhost:8000/inventory/users');
-//   const results = await response.json();
-//   console.log(results)
-// }
-
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
