ab2d56b2183da2c8952641a5e51ef4e800736ec1
Drew Miley <drew.miley@newcastle.ac.uk>
2020-11-06 13:26:47 +0000

    Removes example code from application

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import patientsReducer from '../features/patients/patientsSlice';
-import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     patients: patientsReducer,
   },
 });
