6115ff37f841adaf8088c83dc011cb6b39cc32a6
Drew Miley <drew.miley@newcastle.ac.uk>
2020-11-06 13:59:16 +0000

    Adds basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import { Cohort } from './features/cohort/Cohort';
 import { Patients } from './features/patients/Patients';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Cohort />
-        <Patients />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Cohort View</Link>
+            </li>
+            <li>
+              <Link to='/patients'>Patients View</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path='/patients'>
+            <Patients />
+          </Route>
+          <Route path='/'>
+            <Cohort />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
